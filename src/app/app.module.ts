5a30ae30e7033b8c9ecce699e5af34be6f019f29
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-05-12 04:41:58 +0300

    Pages-error404 correction. Added layouts components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PagesError404Component } from './pages/pages-error404/pages-error404.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { SideBarComponent } from './layouts/side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     PagesError404Component,
+    HeaderComponent,
+    FooterComponent,
+    SideBarComponent,
 
 
 
