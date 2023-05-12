647474ed3ff02024d0d119093db5e02012deeb4f
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-05-12 14:43:10 +0300

    Dashboard UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { PagesError404Component } from './pages/pages-error404/pages-error404.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { SideBarComponent } from './layouts/side-bar/side-bar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SideBarComponent } from './layouts/side-bar/side-bar.component';
     HeaderComponent,
     FooterComponent,
     SideBarComponent,
+    DashboardComponent,
 
 
 
