31777ef0405e92f5da0539106c14cd7baaf9cdc2
Maina Evans <kinuthiafabiankinuthia@gmail.com>
2023-05-13 15:09:09 +0300

    Dashboard revamp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,44 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { PagesError404Component } from './pages/pages-error404/pages-error404.component';
-import { HeaderComponent } from './layouts/header/header.component';
-import { FooterComponent } from './layouts/footer/footer.component';
+
 import { SideBarComponent } from './layouts/side-bar/side-bar.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule } from '@angular/material/divider'
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     PagesError404Component,
-    HeaderComponent,
-    FooterComponent,
     SideBarComponent,
     DashboardComponent,
+
 
 
 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/layouts/side-bar/side-bar.component.ts
+++ src/app/layouts/side-bar/side-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav'
+import { BreakpointObserver } from '@angular/cdk/layout'
 
 @Component({
   selector: 'app-side-bar',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
+  constructor(private observer: BreakpointObserver) { }
+
+  ngAfterViewInit() {
+    this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = "over";
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = "side";
+        this.sidenav.open();
+      }
+    });
+  }
 }
